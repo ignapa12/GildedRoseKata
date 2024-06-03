52a1ef6bb3aae3842b9a7d6a229314d4afe3bb55
Nicolás Hechim <nhechim@voxelgroup.net>
2024-06-03 10:49:10 -0300

    test: decrease sell in after a day

---
+++ GildedRoseTests/GildedRoseShould.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using GildedRoseKata;
+using NUnit.Framework;
+
+namespace GildedRoseTests;
+
+public class GildedRoseShould
+{
+    [Test]
+    public void DecreaseSellInAfterADay()
+    {
+        var sellIn = 2;
+        var items = new List<Item> { new Item { Name = "foo", SellIn = sellIn, Quality = 0 } };
+        var app = new GildedRose(items);
+        app.UpdateQuality();
+        Assert.That(items[0].SellIn, Is.LessThan(sellIn));
+    }
+}
